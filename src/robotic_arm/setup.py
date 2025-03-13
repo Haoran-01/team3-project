7da124d9c9181fa798bc25b5a6faf40c2fb6db3b
hishamx18 <hishu18@gmail.com>
2025-03-13 11:30:58 +0000

    Update setup.py

--- src/robotic_arm/setup.py
+++ src/robotic_arm/setup.py
@@ -1,26 +1,29 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'robotic_arm'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sfr2024',
     maintainer_email='haoran.yan01@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Robotic arm control package for pick-and-place tasks',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'robotic_arm_node = robotic_arm.robotic_arm_node:main'
+    		'px100_pick_and_place = robotic_arm.px100_pick_and_place:main'
         ],
     },
 )
